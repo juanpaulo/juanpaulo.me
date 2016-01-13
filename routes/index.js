870cf592b9132eb7fa032215f8a234c55ca18e86
Juan Paulo Gutierrez <juanpaulo.gutierrez@gmail.com>
2016-01-13 17:31:22 +0900

    added eventually.

--- routes/index.js
+++ routes/index.js
@@ -3,40 +3,40 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'juanpaulo.me'});
+  res.render('eventually', { title: 'juanpaulo.me'});
 });
 
-router.post('/flickrapi', function(req, res) {
-  var Flickr = require('flickrapi'),
-  flickrOptions = {
-    api_key: process.env.API_KEY,
-    secret: process.env.SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  };
-  Flickr.authenticate(flickrOptions, function(error, flickr) {
-    flickr.photosets.getPhotos({
-      photoset_id: '72157622849227887' // Interestingness Album
-    }, function(err, result) {
-      if(err) {
-        throw new Error(err);
-        // TODO res.render('502');
-      }
-      res.send({photos: result.photoset.photo});
-    });
-  });
-});
+// router.post('/flickrapi', function(req, res) {
+//   var Flickr = require('flickrapi'),
+//   flickrOptions = {
+//     api_key: process.env.API_KEY,
+//     secret: process.env.SECRET,
+//     access_token: process.env.ACCESS_TOKEN,
+//     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+//   };
+//   Flickr.authenticate(flickrOptions, function(error, flickr) {
+//     flickr.photosets.getPhotos({
+//       photoset_id: '72157622849227887' // Interestingness Album
+//     }, function(err, result) {
+//       if(err) {
+//         throw new Error(err);
+//         // TODO res.render('502');
+//       }
+//       res.send({photos: result.photoset.photo});
+//     });
+//   });
+// });
 
-router.get('/projects', function(req, res) {
-  res.render('under-construction', { title: 'Projects' });
-});
-
-router.get('/blog', function(req, res) {
-  res.render('under-construction', { title: 'Blog' });
-});
-
-router.get('/resume', function(req, res) {
-  res.render('resume', { title: 'Resume' });
-});
+// router.get('/projects', function(req, res) {
+//   res.render('under-construction', { title: 'Projects' });
+// });
+//
+// router.get('/blog', function(req, res) {
+//   res.render('under-construction', { title: 'Blog' });
+// });
+//
+// router.get('/resume', function(req, res) {
+//   res.render('resume', { title: 'Resume' });
+// });
 
 module.exports = router;
