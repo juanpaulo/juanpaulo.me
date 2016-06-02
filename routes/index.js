6414d1e898e142659877acf498f3bcf61c7051ba
Juan Paulo Gutierrez <juanpaulo.gutierrez@gmail.com>
2016-06-02 10:01:23 +0900

    Added basic healthcheck.

--- routes/index.js
+++ routes/index.js
@@ -6,30 +6,31 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'juanpaulo.me'});
 });
 
-router.post('/flickrapi', function(req, res) {
-  var Flickr = require('flickrapi'),
-  flickrOptions = {
-    api_key: process.env.API_KEY,
-    secret: process.env.SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  };
-  Flickr.authenticate(flickrOptions, function(error, flickr) {
-    flickr.photosets.getPhotos({
-      photoset_id: '72157622849227887' // Interestingness Album
-    }, function(err, result) {
-      if(err) {
-        throw new Error(err);
-        // TODO res.render('502');
-      }
-      res.send({photos: result.photoset.photo});
-    });
-  });
+// Availability monitoring
+router.get('/ping', function(req, res) {
+  res.send('pong');
 });
 
-router.get('/projects', function(req, res) {
-  res.render('under-construction', { title: 'Projects' });
-});
+// router.post('/flickrapi', function(req, res) {
+//   var Flickr = require('flickrapi'),
+//   flickrOptions = {
+//     api_key: process.env.API_KEY,
+//     secret: process.env.SECRET,
+//     access_token: process.env.ACCESS_TOKEN,
+//     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+//   };
+//   Flickr.authenticate(flickrOptions, function(error, flickr) {
+//     flickr.photosets.getPhotos({
+//       photoset_id: '72157622849227887' // Interestingness Album
+//     }, function(err, result) {
+//       if(err) {
+//         throw new Error(err);
+//         // TODO res.render('502');
+//       }
+//       res.send({photos: result.photoset.photo});
+//     });
+//   });
+// });
 
 router.get('/blog', function(req, res) {
   res.render('under-construction', { title: 'Blog' });
